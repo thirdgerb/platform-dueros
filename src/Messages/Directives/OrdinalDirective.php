cbe53755abbc0abef954cd24222a76b1faca898c
thirdgerb <thirdgerb@gmail.com>
2019-12-31 00:28:30 +0800

    fix 尝试排查dueros response 不通过的问题

--- src/Messages/Directives/OrdinalDirective.php
+++ src/Messages/Directives/OrdinalDirective.php
@@ -71,10 +71,6 @@ class OrdinalDirective extends AbsDirective
         $i = 0;
         foreach ($this->suggestions as $index => $suggestion) {
             $i++;
-
-            if (is_string($index)) {
-                $options[] = $this->wrapOption($i, $index);
-            }
             $options[] = $this->wrapOption($i, $suggestion);
         }
 
